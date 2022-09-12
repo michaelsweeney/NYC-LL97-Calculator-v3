c087cd1a2279dbadc8125d1b4d3b14603c978c3a
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2022-09-12 17:36:44 -0400

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,7 @@ import MainLayout from "components/mainlayout";
 import InfoModal from "components/modals/infomodal";
 import LoadModal from "components/modals/loadmodal";
 import BuildingSummaryModal from "components/modals/buildingsummarymodal";
-
-import { useAppDispatch, useAppSelector } from "store/hooks";
-
-import { uiActions } from "store/uislice";
-import { buildingInputActions } from "store/buildinginputslice";
-import { LL84SelectionToLL97Inputs } from "locallaw/ll84_to_ll97";
+import TestComponent from "testcomponent";
 
 const theme = createTheme({
   palette: {
@@ -26,34 +21,9 @@ const theme = createTheme({
 });
 
 const App = () => {
-  // testing
-
-  const building_inputs = useAppSelector((state) => state.building_inputs);
-  const ll84_query_results = useAppSelector(
-    (state) => state.ui.ll84_query_results
-  );
-
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(uiActions.setLL84QueryInput("On"));
-    dispatch(uiActions.setLL84YearSelection("ll84_2021_cal_2020"));
-
-    let selected_ll84_data = ll84_query_results[0];
-
-    dispatch(uiActions.setSelectedLL84Property(selected_ll84_data));
-    dispatch(uiActions.setIsLoadModalOpen(false));
-
-    let ll97_inputs = LL84SelectionToLL97Inputs(selected_ll84_data);
-
-    dispatch(
-      buildingInputActions.setBuildingInputsFromLL84Results(ll97_inputs)
-    );
-
-    // dispatch loaded inputs to building inputs.
-  }, [building_inputs, ll84_query_results, dispatch]);
-
   return (
     <ThemeProvider theme={theme}>
+      <TestComponent />
       <InfoModal />
       <LoadModal />
       <BuildingSummaryModal />
---
+++ src/testcomponent.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+
+import { useAppDispatch, useAppSelector } from "store/hooks";
+
+import { uiActions } from "store/uislice";
+import { buildingInputActions } from "store/buildinginputslice";
+import { LL84SelectionToLL97Inputs } from "locallaw/ll84_to_ll97";
+
+const TestComponent = () => {
+  // testing
+
+  const building_inputs = useAppSelector((state) => state.building_inputs);
+  const ll84_query_results = useAppSelector(
+    (state) => state.ui.ll84_query_results
+  );
+
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(uiActions.setLL84QueryInput("On"));
+    dispatch(uiActions.setLL84YearSelection("ll84_2021_cal_2020"));
+
+    let selected_ll84_data = ll84_query_results[0];
+
+    dispatch(uiActions.setSelectedLL84Property(selected_ll84_data));
+    dispatch(uiActions.setIsLoadModalOpen(false));
+
+    let ll97_inputs = LL84SelectionToLL97Inputs(selected_ll84_data);
+
+    dispatch(
+      buildingInputActions.setBuildingInputsFromLL84Results(ll97_inputs)
+    );
+
+    // dispatch loaded inputs to building inputs.
+  }, [building_inputs, ll84_query_results, dispatch]);
+
+  useEffect(() => {}, []);
+  return <div sx={{ display: "none" }}></div>;
+};
+
+export default TestComponent;
