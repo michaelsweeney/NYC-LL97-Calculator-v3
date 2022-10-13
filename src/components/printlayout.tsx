bf6aeddfad978e75a48dc17c9f00bebcc34d5ddb
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2022-10-13 10:59:16 -0400

    start-print-mode

--- src/components/printlayout.tsx
+++ src/components/printlayout.tsx
@@ -1,14 +1,41 @@
-
-import * as react from 'react'
+import * as react from "react";
 
 import { useAppDispatch, useAppSelector } from "store/hooks";
 
+import { Dialog, Button } from "@mui/material";
 
+import { CloseIconButton } from "./icons";
+import { ButtonPrimary } from "styles/components";
+import { MenuTitle } from "styles/typography";
+import { InlineStylesType } from "types";
+import { colors } from "styles/colors";
+import { uiActions } from "store/uislice";
+
+const styles: InlineStylesType = {
+  root: {},
+};
 
 const PrintLayout = () => {
-    return <div></div>
-}
+  const { is_print_mode } = useAppSelector((state) => state.ui);
+  const dispatch = useAppDispatch();
 
+  const exitPrintMode = () => {
+    dispatch(uiActions.setIsPrintMode(false));
+  };
 
+  return (
+    <Dialog
+      open={is_print_mode}
+      fullWidth={true}
+      onClose={exitPrintMode}
+      maxWidth="xl"
+    >
+      <div style={styles.root}>
+        PRINT MODE HERE
+        <Button onClick={exitPrintMode}>CLOSE</Button>
+      </div>
+    </Dialog>
+  );
+};
 
-export default PrintLayout
+export default PrintLayout;
