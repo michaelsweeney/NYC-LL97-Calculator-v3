e247f69fdbda3c826be3c077bc7b2382814995e9
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2022-12-06 11:44:53 -0500

    logo-link

--- src/components/calclogo.tsx
+++ src/components/calclogo.tsx
@@ -12,7 +12,11 @@ const CalcLogo = (props: PropTypes) => {
         boxSizing: "border-box",
       }}
     >
-      <a href="https://be-exchange.org" target="_blank" rel="noreferrer">
+      <a
+        href="https://be-exchange.org/ll97-calculator/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img width={width} alt="a" src="beex_CC_art_webheader.webp"></img>
       </a>
     </div>
